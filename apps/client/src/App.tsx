801497388eecfede6a4af3134ef36475e26f4ca7
Dominykas Bartkus <dominykasbart@gmail.com>
2023-07-08 15:52:47 +0300

    Cleanup && SolidJS init

---
+++ apps/client/src/App.tsx
@@ -0,0 +1,9 @@
+import type { Component } from "solid-js";
+
+export const App: Component = () => {
+  return (
+    <div class="w-screen h-screen">
+      <h1 class="font-bold text-3xl">Sup!</h1>
+    </div>
+  );
+};
